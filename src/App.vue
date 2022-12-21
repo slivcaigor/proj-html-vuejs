535784a7ac1e4745c0114262bb5ea03310d0d5e3
slivcaigor <slivcaigor@gmail.com>
2022-12-21 12:11:40 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import Header from './components/Header.vue'
+import HomeMain from './components/HomeMain.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    HomeMain,
+    Footer
+  },
+};
+</script>
+
+<template>
+  <Header />
+  <HomeMain />
+  <Footer />
+</template>
+
+<style lang="scss" scoped>
+@use './styles/general.scss' as *;
+@use './styles/partials/mixins' as *;
+@use './styles/partials/variables' as *;
+</style>
---
+++ src/components/HomeMain.vue
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+</style>
